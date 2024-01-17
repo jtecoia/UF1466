7af0b43954958313a9c25fda39891a9be978ee92
Jesus <jtecoia@gmail.com>
2024-01-17 19:43:23 +0100

    Examen UF1466 Final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,81 @@
+class Vehiculo {
+    // Atributos
+    String marca;
+    String modelo;
+    int anoFabricacion;
 
+    // Constructor
+    public Vehiculo(String marca, String modelo, int anoFabricacion) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anoFabricacion = anoFabricacion;
+    }
+
+    // Método mostrarDetalles
+    public void mostrarDetalles() {
+        System.out.println("Detalles del vehículo:");
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Año de fabricación: " + anoFabricacion);
+    }
+}
+
+class VehiculoMotorizado extends Vehiculo {
+    // Atributo adicional
+    String tipoCombustible;
+
+    // Constructor
+    public VehiculoMotorizado(String marca, String modelo, int anoFabricacion, String tipoCombustible) {
+        super(marca, modelo, anoFabricacion);
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    // Método mostrarDetalles
+    @Override
+    public void mostrarDetalles() {
+        super.mostrarDetalles();
+        System.out.println("Tipo de combustible: " + tipoCombustible);
+    }
+}
+
+class Automovil extends VehiculoMotorizado {
+    // Atributo adicional
+    int numeroPuertas;
+
+    // Constructor
+    public Automovil(String marca, String modelo, int anoFabricacion, String tipoCombustible, int numeroPuertas) {
+        super(marca, modelo, anoFabricacion, tipoCombustible);
+        this.numeroPuertas = numeroPuertas;
+    }
+
+    // Método mostrarDetalles
+    @Override
+    public void mostrarDetalles() {
+        super.mostrarDetalles();
+        System.out.println("Número de puertas: " + numeroPuertas);
+    }
+}
+
+class Motocicleta extends VehiculoMotorizado {
+    // Atributo adicional
+    String tipoMotocicleta;
+
+    // Constructor
+    public Motocicleta(String marca, String modelo, int anoFabricacion, String tipoCombustible, String tipoMotocicleta) {
+        super(marca, modelo, anoFabricacion, tipoCombustible);
+        this.tipoMotocicleta = tipoMotocicleta;
+    }
+
+    // Método mostrarDetalles
+    @Override
+    public void mostrarDetalles() {
+        super.mostrarDetalles();
+        System.out.println("Tipo de motocicleta: " + tipoMotocicleta);
+    }
+}
 
 public class Main {
-    public static void AplicacionConsola(String[] args) {
+    public static void main(String[] args) {
         // Crear instancias de diferentes tipos de vehículos
         Vehiculo vehiculo = new Vehiculo("Toyota", "Camry", 2022);
         VehiculoMotorizado vehiculoMotorizado = new VehiculoMotorizado("Honda", "Civic", 2021, "Gasolina");
@@ -18,4 +92,3 @@ public class Main {
         motocicleta.mostrarDetalles();
     }
 }
-
